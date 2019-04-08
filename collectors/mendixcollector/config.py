3f307eec7fbb0df96a7373916a30e83c77997b7b
anay.latiwal@gmail.com <root@ip-10-210-192-209.us-east-2.compute.internal>
2019-04-08 07:10:48 +0000

    new update

--- collectors/mendixcollector/config.py
+++
@@ -1,6 +0,0 @@
-Mendix_collector = {
-        'username': 'jason.bartlett@erieinsurance.com',
-        'apikey': 'c2374073-2395-47a1-8c79-14e8788ec2aa',
-        'db_host': 'localhost',
-        'db_port': 27017
-        }
